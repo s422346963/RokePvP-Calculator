18597f3124d7a703039ccafde7cdf0a0a7d122f7
qiangbin <422346963@163.com>
2026-05-24 22:16:59 +0800

    Init

---
+++ analytics.js
@@ -0,0 +1,15 @@
+
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?49d49c109b13fc4469f9bb0950d0d3ea";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+(function () {
+    var footer = document.createElement("footer");
+    footer.style.cssText = "position: fixed; bottom: 10px; width: 100%; text-align: center; font-size: 14px; color: #666;";
+    footer.innerHTML = '备案号：<a href="https://beian.miit.gov.cn/" target="_blank" style="color: #666; text-decoration: none;">津ICP备2023006150号</a>';
+    document.body.appendChild(footer);
+})();
